843f5876edc9e7914e7b565be1f6f8faddca40a2
UpdeshVR <164134879+UpdeshVR@users.noreply.github.com>
2024-06-26 12:07:51 +0530

    tap to place change

--- tap-place-cursor.js
+++ tap-place-cursor.js
@@ -1,20 +1,6 @@
 // Component that places trees at cursor location when screen is tapped
 const tapPlaceCursorComponent = () => ({
   init() {
-    const place = document.getElementById("btn3");
-    const nextbutton = document.getElementById("nextbutton");
-    const viewbtn = document.getElementById("view-btn");
-    const startScreen = document.querySelector(".start-screen");
-    const arScreen = document.querySelector(".ar-screen");
-    const instructioninfo = document.querySelector(".instruction-info");
-
-    const carouselcontainer = document.querySelector(".carousel-container");
-    // const reduceBending = document.querySelector('#reduceBending')
-    const bendmodel = document.getElementById("bend-model");
-    const model = document.getElementById("model");
-    const bendui = document.getElementById("bendui_1_model");
-
-    const scene = this.el.sceneEl;
     this.raycaster = new THREE.Raycaster();
     this.camera = document.getElementById("camera");
     this.threeCamera = this.camera.getObject3D("camera");
@@ -29,61 +15,21 @@ const tapPlaceCursorComponent = () => ({
     this.rayOrigin = new THREE.Vector2(0, 0);
 
     this.cursorLocation = new THREE.Vector3(0, 0, 0);
-    viewbtn.addEventListener("click", () => {
-      startScreen.style.display = "none";
-      arScreen.style.display = "block";
-    });
-    function handleArScreenClick() {
-      scene.emit("recenter");
-      console.log("cursor clicked");
-    }
-    place.addEventListener("click", () => {
+
+    this.el.sceneEl.addEventListener("click", (event) => {
       if (hasPlacedModel !== true) {
         hasPlacedModel = true;
-        instructioninfo.style.display = "flex";
 
         this.model.setAttribute("position", this.el.object3D.position);
         this.model.setAttribute("visible", "true");
-        this.model.setAttribute("rotation", "0 0 0");
-        // scene.emit('recenter')
 
-        this.cursor.setAttribute("visible", "false");
+        // Remove ghosted model from scene after model is placed
+        this.cursor.parentNode.removeChild(this.cursor);
 
         // Add raycaster to camera
         this.camera.setAttribute("raycaster", "objects: .cantap");
-        place.style.display = "none";
-        arScreen.style.pointerEvents = "none";
-        arScreen.removeEventListener("click", handleArScreenClick);
-
-        nextbutton.style.display = "block";
-        setTimeout(() => {
-          carouselcontainer.style.visibility = "visible";
-          carouselcontainer.style.opacity = "1";
-          carouselcontainer.style.pointerEvents = "auto";
-          // reduceBending.style.display = 'block'
-          instructioninfo.style.display = "none";
-          model.setAttribute("animation-mixer", {
-            clip: "bend",
-            loop: "once",
-            crossFadeDuration: 0.4,
-          });
-          bendmodel.setAttribute("animation-mixer", {
-            clip: "bend",
-            loop: "once",
-            crossFadeDuration: 0.4,
-          });
-          bendui.setAttribute("animation-mixer", {
-            clip: "bend",
-            loop: "once",
-            crossFadeDuration: 0.4,
-          });
-        }, 4000);
-
-        console.log("Play Animation 0");
-        // scene.emit('recenter')
       }
     });
-    arScreen.addEventListener("click", handleArScreenClick);
   },
   tick() {
     // Raycast from camera to 'ground'
