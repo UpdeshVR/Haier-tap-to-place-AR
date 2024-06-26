5dd8965d07d8e60ea3b18b63769419933a4ed9a4
UpdeshVR <164134879+UpdeshVR@users.noreply.github.com>
2024-06-26 11:31:58 +0530

    changes are done

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+const navbar = document.querySelector('.nav')
+const fridge = document.querySelector('#fridge')
+
+
+const adjustFridgeHeight = () => {
+    const navbarHeight = navbar.offsetHeight;
+    fridge.style.maxHeight = `calc(68dvh - ${navbarHeight}px)`
+}
+
+adjustFridgeHeight()
+
+document.addEventListener("DOMContentLoaded", () => {
+    adjustFridgeHeight()
+})
+
+document.addEventListener('load', () => {
+    adjustFridgeHeight()
+})
+
+document.addEventListener('resize', () => {
+    adjustFridgeHeight()
+})
